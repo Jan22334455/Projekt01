5c58648f0a60bcdd5c77debf1efde6f9cee12aab
Jan22334455 <Jan-Frederik@gmx.net>
2019-01-02 21:25:16 +0100

    Revert "Revert "Initial commit""
    This reverts commit ee97a1a33407cc62d69ecdaf4e742cbae3af1d5d.

---
+++ src/Berührung/Main.java
@@ -0,0 +1,36 @@
+package Berührung;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    private static Stage s1;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Berührung.fxml"));
+        primaryStage.setTitle("SuperTollesSpiel2");
+        primaryStage.setMaxHeight(500);
+        primaryStage.setMaxWidth(750);
+        primaryStage.setMinHeight(500);
+        primaryStage.setMinWidth(750);
+        primaryStage.setScene(new Scene(root, 750, 500));
+        primaryStage.requestFocus();
+        primaryStage.setOnCloseRequest(e -> System.exit(0));
+        primaryStage.show();
+        s1 = primaryStage;
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public static Stage getS1() {
+        return s1;
+    }
+}
